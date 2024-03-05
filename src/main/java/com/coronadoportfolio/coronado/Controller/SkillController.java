5139918adf7e3c7cf2dbbc3261792f528ee5210e
CoronadoW <w_cicerone@hotmail.com>
2024-03-04 22:52:27 -0300

    intento de hacer el deploy

--- src/main/java/com/coronadoportfolio/coronado/Controller/SkillController.java
+++ src/main/java/com/coronadoportfolio/coronado/Controller/SkillController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("skill")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SkillController {
     @Autowired
     SkillService skillService;
--- src/main/java/com/coronadoportfolio/coronado/Controller/SoftSkillController.java
+++ src/main/java/com/coronadoportfolio/coronado/Controller/SoftSkillController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("softSkill")
-@CrossOrigin(origins = "https://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SoftSkillController {
     
     @Autowired
